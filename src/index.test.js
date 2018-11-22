65919575c3387e56b951f75dce06e7d9dc283015
Arthur P <tech@apduarte.com>
2018-11-21 20:45:48 -0800

    Demo: DOM Testing

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,21 @@
 import { expect } from 'chai';
+import { JSDOM } from 'jsdom';
+import fs from 'fs';
 
 describe('Our first test', () => {
     it('should pass', () => {
         expect(true).to.equal(true);
-    })
+    });
+});
+
+describe('index.html', () => {
+    it('shoud say hello', () => {
+        const index = fs.readFileSync('./src/index.html', 'utf-8');
+        const window = new JSDOM(index).window;
+
+        const h1 = window.document.getElementsByTagName('h1')[0];
+        expect(h1.innerHTML).to.equal('Hello World?');
+
+        window.close();
+    });
 });
